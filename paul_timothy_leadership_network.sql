cceeb18fa002090c252d8a052b527d32de6713f4
raymondaffedzie@protonmail.com <raymondaffedzie@protonmail.com>
2022-07-12 13:41:04 +0000

    new commit

--- paul_timothy_leadership_network.sql
+++ paul_timothy_leadership_network.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2022 at 09:18 PM
+-- Generation Time: Jul 12, 2022 at 03:35 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -36,35 +36,20 @@ CREATE TABLE `admin_registration` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `pwd_reset_token` int(11) NOT NULL,
-  `status` text NOT NULL DEFAULT 'Moderator'
+  `status` text NOT NULL DEFAULT 'Moderator',
+  `check_status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin_registration`
 --
 
-INSERT INTO `admin_registration` (`id`, `firstname`, `surname`, `username`, `phone_number`, `email`, `password`, `pwd_reset_token`, `status`) VALUES
-(1, 'George Kwamina', 'Ntsiful', 'jerry', '0247834206', 'georgentsifulkwamina@gmail.com', '$2y$10$apl732qNFzcuMQHN1mv9QO8vcr.ABQyAUV3QIBN/YKUdFkUR25w6y', 0, 'Administrator'),
-(2, 'Raymond', 'Affedzie', 'berth_ray', '0247692388', 'raymondaffedzie@gmail.com', '$2y$10$U7Pnn2F/Zg1Fb4SIZueLieQ2j69aS3axPNBZ3tdgME4Vhp76j8p62', 467999, 'Administrator'),
-(3, 'Stephen', 'Mensah', 'stephy', '0553053239', 'stephenymensah@gmail.com', '$2y$10$yNjrJvHTn8b0j0NIygCyHO2BKZ.K4rtlvRI8vw53TQOV/fVq55y2W', 769504, 'Administrator'),
-(4, 'Jane', 'Doe', 'janedoe', '0123456789', 'janedoe@email.com', '$2y$10$EUvq3w9ZIDvBgZjdoZ0HiO.B.WaRfUUnxNhVXeS5J96CDYpS0/Jxq', 0, 'Moderator');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `be_part_of_us`
---
-
-CREATE TABLE `be_part_of_us` (
-  `member_id` varchar(15) NOT NULL,
-  `first_name` varchar(25) NOT NULL,
-  `surname` varchar(35) NOT NULL,
-  `email` varchar(65) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
-  `means_of_contact` varchar(15) NOT NULL,
-  `category_of_joining` varchar(20) NOT NULL,
-  `status` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `admin_registration` (`id`, `firstname`, `surname`, `username`, `phone_number`, `email`, `password`, `pwd_reset_token`, `status`, `check_status`) VALUES
+(1, 'George', 'Ntsiful', 'jerry', '0247834206', 'georgentsifulk@gmail.com', '$2y$10$apl732qNFzcuMQHN1mv9QO8vcr.ABQyAUV3QIBN/YKUdFkUR25w6y', 0, 'Administrator', 'Active'),
+(2, 'Raymond', 'Affedzie', 'berth_ray', '0247692388', 'raymondaffedzie@gmail.com', '$2y$10$U7Pnn2F/Zg1Fb4SIZueLieQ2j69aS3axPNBZ3tdgME4Vhp76j8p62', 467999, 'Administrator', 'Active'),
+(3, 'Stephen', 'Mensah', 'stephy', '0553053239', 'stephenymensah@gmail.com', '$2y$10$yNjrJvHTn8b0j0NIygCyHO2BKZ.K4rtlvRI8vw53TQOV/fVq55y2W', 769504, 'Administrator', 'Active'),
+(4, 'Jane', 'Doe', 'janedoe', '0123456789', 'janedoe@email.com', '$2y$10$EUvq3w9ZIDvBgZjdoZ0HiO.B.WaRfUUnxNhVXeS5J96CDYpS0/Jxq', 0, 'Moderator', 'Active'),
+(5, 'John', 'Doe', 'john', '+0912345678', 'johndoe@email.com', '$2y$10$1JZNRTKht05m7kBxYnlH/.A9DFF5iiHlGw7KQJiW6q74920/Ulioq', 0, 'Moderator', 'Inactive');
 
 -- --------------------------------------------------------
 
@@ -86,7 +71,7 @@ CREATE TABLE `carousel` (
 --
 
 CREATE TABLE `events` (
-  `event_id` varchar(15) NOT NULL,
+  `event_id` int(11) NOT NULL,
   `tittle` varchar(200) NOT NULL,
   `content` varchar(1000) NOT NULL,
   `date` date NOT NULL,
@@ -94,10 +79,17 @@ CREATE TABLE `events` (
   `venue` varchar(200) NOT NULL,
   `town` varchar(200) NOT NULL,
   `country` varchar(200) NOT NULL,
-  `info-email` varchar(60) DEFAULT NULL,
-  `info-contact` varchar(20) DEFAULT NULL,
-  `admin_id` varchar(15) NOT NULL
+  `info_email` varchar(60) DEFAULT NULL,
+  `info_contact` varchar(20) DEFAULT NULL,
+  `admin_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `tittle`, `content`, `date`, `time`, `venue`, `town`, `country`, `info_email`, `info_contact`, `admin_id`) VALUES
+(1, 'Girls In Information Technology Training', '1. Web Development 2. Multimedia authoring (Adobe photoshop, Adobe premiere) 3. Microsoft Office Suite', '2022-07-25', '07:30:00', 'Irrba Training Center', 'Takoradi', 'Ghana', 'irrbawebsdev@gmail.com', '0205823707', 2);
 
 -- --------------------------------------------------------
 
@@ -109,6 +101,23 @@ CREATE TABLE `events_likes` (
   `evntlike_id` varchar(15) NOT NULL,
   `event_id` varchar(15) NOT NULL,
   `member_id` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `member_id` varchar(15) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `surname` varchar(35) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `means_of_contact` varchar(15) NOT NULL,
+  `category_of_joining` varchar(20) NOT NULL,
+  `status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -131,14 +140,21 @@ CREATE TABLE `motivation` (
 --
 
 CREATE TABLE `news` (
-  `new_id` varchar(15) NOT NULL,
-  `tittle` varchar(200) NOT NULL,
+  `new_id` int(11) NOT NULL,
+  `tittle` text NOT NULL,
   `content` longtext NOT NULL,
-  `publisher_name` varchar(60) NOT NULL,
-  `admin_id` varchar(15) NOT NULL,
+  `publisher_name` text NOT NULL,
+  `admin_id` int(11) NOT NULL,
   `date` date NOT NULL,
   `source` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`new_id`, `tittle`, `content`, `publisher_name`, `admin_id`, `date`, `source`) VALUES
+(1, '39 year Old Black Entrepreneur Makes History, Awarded $13.4 Billion Dollar Defense Contract', 'Meet Isaac Barnes, the young founder and President of Eminent Future, whose Black-owned tech firm has been awarded a $13.4 billion dollar defense contract with the U.S. Airforce and the U.S. Spaceforce.Isaac is a marvel, reminiscent of young Black leaders transcending generations of relevant and personable individuals who have made such an extraordinary mark in history.Hailing from Grand Rapids, Michigan, and growing up disadvantaged yet determined, Isaac’s saga is one recognizable, with an unfamiliar progression. After dropping out of college, he would serve piously with the US Marines, where he prospered as a software engineer and data analyst supporting the Commandant of the Marine Corps. Isaac then went on to work for the Secretary of Defense to produce federal websites and digital products that saved millions of dollars for the Department of Defense.Chris Brown Breaks Two Billboard Records With Latest ‘Breezy’ ReleaseIsaac received numerous awards and accolades for his innovations. His passion for technology and being a steward for the people led him to serve with influence under both the Obama and Trump administrations. His team led the 2017 presidential records transition efforts for President Obama.Notwithstanding his apparent success and still seeking the need to serve by leading, Isaac went on to be the first Black multi-millionaire President of a Federal Digital Product and Innovation Company, Eminent Future. Serving as the President of Eminent Future, he has been paramount in securing a defense contract worth more than $13.4 billion whilst positioning the company to be one of the fastest-growing companies in Arlington, Virginia.Isaac emphatically states, “The biggest issue that we have in America is that we are not working together as one unit; we are not combined.” Isaac combines leadership, entrepreneurship, technology, and spirituality to design growth opportunities within and between organizations and inclusive of communities to generate and instill that cohesive unity in America.On the heels of his remarkable accomplishments, Isaac, in conjunction with business partner Jose Risi established two crypto tokens, xMooney, and a stealth project. xMooney encourages its miners to reduce their carbon footprint while ensuring a more stable and secure blockchain.Now, Isaac is using his platform and resources to give back. He is a vocal advocate for diversity in tech and is working to close the black tech gap. His story inspires anyone who wants to make a difference in the world. He believes cryptocurrency and Web3 are the future, and he is creating pathways for more black and brown people to join the movement.', 'BLACK ENTERPRISE Editors', 2, '2022-07-12', 'www.blackenterprice.com');
 
 -- --------------------------------------------------------
 
@@ -192,8 +208,8 @@ INSERT INTO `password_reset` (`pwd_reset_id`, `pwd_reset_email`, `pwd_reset_toke
 --
 
 CREATE TABLE `story` (
-  `story_id` varchar(15) NOT NULL,
-  `tittle` varchar(100) NOT NULL,
+  `story_id` int(11) NOT NULL,
+  `tittle` text NOT NULL,
   `content` longtext NOT NULL,
   `date` date NOT NULL,
   `country` varchar(30) NOT NULL,
@@ -279,12 +295,6 @@ ALTER TABLE `admin_registration`
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- Indexes for table `be_part_of_us`
---
-ALTER TABLE `be_part_of_us`
-  ADD PRIMARY KEY (`member_id`);
-
---
 -- Indexes for table `carousel`
 --
 ALTER TABLE `carousel`
@@ -303,6 +313,12 @@ ALTER TABLE `events`
 ALTER TABLE `events_likes`
   ADD PRIMARY KEY (`evntlike_id`),
   ADD UNIQUE KEY `member_id` (`member_id`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`member_id`);
 
 --
 -- Indexes for table `motivation`
@@ -382,7 +398,19 @@ ALTER TABLE `video_library`
 -- AUTO_INCREMENT for table `admin_registration`
 --
 ALTER TABLE `admin_registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `new_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `password_reset`
@@ -391,52 +419,26 @@ ALTER TABLE `password_reset`
   MODIFY `pwd_reset_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for table `story`
 --
+ALTER TABLE `story`
+  MODIFY `story_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for table `events_likes`
+-- Constraints for dumped tables
 --
-ALTER TABLE `events_likes`
-  ADD CONSTRAINT `events_likes_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `news_comments`
 --
 ALTER TABLE `news_comments`
-  ADD CONSTRAINT `news_comments_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `news_comments_ibfk_2` FOREIGN KEY (`news_id`) REFERENCES `news` (`new_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `news_like`
---
-ALTER TABLE `news_like`
-  ADD CONSTRAINT `news_like_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `story_approval`
---
-ALTER TABLE `story_approval`
-  ADD CONSTRAINT `story_approval_ibfk_1` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `story_comments`
---
-ALTER TABLE `story_comments`
-  ADD CONSTRAINT `story_comments_ibfk_1` FOREIGN KEY (`story_id`) REFERENCES `story` (`story_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `story_comments_ibfk_2` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `story_likes`
---
-ALTER TABLE `story_likes`
-  ADD CONSTRAINT `story_likes_ibfk_2` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `news_comments_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `subcriptions`
 --
 ALTER TABLE `subcriptions`
-  ADD CONSTRAINT `subcriptions_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `be_part_of_us` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `subcriptions_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
